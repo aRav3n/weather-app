d30b5296695e37ff289ac99be1b368980f4e8578
aRav3n <24660772+aRav3n@users.noreply.github.com>
2024-01-04 15:14:29 -0600

    Add pertinent data to new object

--- script.js
+++ script.js
@@ -10,12 +10,45 @@ const generateUrl = function () {
   }
 };
 
-const getJson = async function () {
+const parcelData = async function (object) {
+  try {
+    const location = object.location;
+    const current = object.current;
+    const condition = current.condition;
+    let weatherData = {};
+
+    const addToDataObject = function (key, dataPiece) {
+      weatherData[key] = dataPiece;
+    };
+
+    addToDataObject("country", location.country);
+    addToDataObject("city", location.name);
+    addToDataObject("conditionText", condition.text);
+    const conditionImage = new Image();
+    conditionImage.src = condition.icon;
+    addToDataObject("conditionImage", conditionImage);
+    addToDataObject("cloudPercent", current.cloud);
+    addToDataObject("tempC", current.temp_c);
+    addToDataObject("tempF", current.temp_f);
+    addToDataObject("windMph", current.wind_mph);
+    addToDataObject("windKph", current.wind_kph);
+    addToDataObject("humidity", current.humidity);
+    addToDataObject("feelsLikeC", current.feelslike_c);
+    addToDataObject("feelsLikeF", current.feelslike_f);
+    return weatherData;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getData = async function () {
   try {
     const url = generateUrl();
     const response = await fetch(url, { mode: "cors" });
-    const responseJson = response.json();
-    return responseJson;
+    const responseJson = await response.json();
+    const weatherData = await parcelData(responseJson);
+    console.log(weatherData);
+    return weatherData;
   } catch (error) {
     console.log(error);
   }
