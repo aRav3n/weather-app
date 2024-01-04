4191f697e17c2fd7904bef9eefbe8269f1633244
aRav3n <24660772+aRav3n@users.noreply.github.com>
2024-01-04 10:38:49 -0600

    Write function to get weather JSON

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 let city = "Merida, Mexico";
 
-const generateUrl = async function (city) {
+const generateUrl = function () {
   try {
     const apiKey = "0cdd91de3c714477a03190112240201";
-    const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
+    const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=no`;
     return url;
   } catch (error) {
     console.log(error);
   }
 };
+
+const getJson = async function () {
+  try {
+    const url = generateUrl();
+    const response = await fetch(
+      url, 
+      { mode: "cors" }
+      );
+    const responseJson = response.json();
+    console.log(responseJson);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+let url = generateUrl();
+getJson(url);
